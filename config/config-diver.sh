2cf1f5fb509974c2e62fd43fbe0c3092f8e6d145
cryptotronxyz <40378575+cryptotronxyz@users.noreply.github.com>
2021-02-20 17:20:39 -0700

    Create config-diver.sh

---
+++ config/config-diver.sh
@@ -0,0 +1,32 @@
+function setMainVars() {
+## set network dependent variables
+NETWORK=""
+NODE_USER=${FORK}${NETWORK}
+COINPORT=16178
+COINRPCPORT=16174
+COINAPIPORT=37221
+}
+
+function setTestVars() {
+## set network dependent variables
+NETWORK="-testnet"
+NODE_USER=${FORK}${NETWORK}
+COINPORT=26178
+COINRPCPORT=26174
+COINAPIPORT=38221
+}
+
+function setGeneralVars() {
+## set general variables
+COINRUNCMD="dotnet ./Stratis.StratisD.dll ${NETWORK} -datadir=/home/${NODE_USER}/.${FORK}node -maxblkmem=2 \${stakeparams}"
+COINGITHUB=https://github.com/stratisproject/StratisBitcoinFullNode.git
+COINDSRC=/home/${NODE_USER}/code/src/Stratis.StratisD
+CONF=release
+COINDAEMON=${FORK}d
+COINCONFIG=${FORK}.conf
+COINSTARTUP=/home/${NODE_USER}/${FORK}d
+COINDLOC=/home/${NODE_USER}/${FORK}node
+COINSERVICELOC=/etc/systemd/system/
+COINSERVICENAME=${COINDAEMON}@${NODE_USER}
+SWAPSIZE="1024" ## =1GB
+}
